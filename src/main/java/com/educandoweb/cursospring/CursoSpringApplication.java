11a02ac1e3a16ef83beb1331be89c520a2b09daa
Gustavo Alves Abreu <113162142+Gustavoabreu94@users.noreply.github.com>
2022-10-07 12:15:32 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/educandoweb/cursospring/CursoSpringApplication.java
+++ src/main/java/com/educandoweb/cursospring/CursoSpringApplication.java
@@ -3,12 +3,15 @@ package com.educandoweb.cursospring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.educandoweb.cursospring.entities.User;
+
 @SpringBootApplication
 public class CursoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
 		
+
 
 	}
 
