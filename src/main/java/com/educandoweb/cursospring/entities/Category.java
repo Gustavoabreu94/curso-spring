99e950c920b342b2a3d5ee00a60328b1a7708244
Gustavo Alves Abreu <113162142+Gustavoabreu94@users.noreply.github.com>
2022-10-09 14:32:07 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/cursospring/entities/Category.java
+++ src/main/java/com/educandoweb/cursospring/entities/Category.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,6 +28,7 @@ public class Category implements Serializable {
 	private String name;
 	
 	@ManyToMany(mappedBy = "categories")
+	@JsonIgnore
 	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
