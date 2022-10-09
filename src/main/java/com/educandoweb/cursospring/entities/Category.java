4970b78f5624eb4034fd43caf88b907644dd2704
Gustavo Alves Abreu <113162142+Gustavoabreu94@users.noreply.github.com>
2022-10-09 13:55:25 -0300

    Many-tomany association with JoinTable

--- src/main/java/com/educandoweb/cursospring/entities/Category.java
+++ src/main/java/com/educandoweb/cursospring/entities/Category.java
@@ -9,8 +9,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -23,7 +25,7 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
-	@Transient
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
